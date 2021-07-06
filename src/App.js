c6189cd7d7a9c155fa04697943fe6118bf2af02d
ChrysenPark <chrysen777@gmail.com>
2021-07-06 13:18:32 -0700

    backk5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import history from "./history";
+import Button from "./Components/Buttons";
+import React from "react";
+import Wrapper from "./Components/Wrapper";
+import image from "./theJarBar.png";
+import Message from "./Components/Message/";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Message message={""} />
+      <div className="Home">
+        <img src={image} alt="jars" width="200" height="auto" />
+        <div className="lander">
+          <Message message={""} />
+          <form>
+            <Button
+              type="button"
+              buttonStyle="btn--solid--solid"
+              buttonSize="btn-small"
+              onClick={() => history.push("/login")}
+            >
+              Login
+            </Button>
+            &nbsp;&nbsp;&nbsp;
+            <Button
+              type="button"
+              buttonStyle="btn--rank--solid"
+              buttonSize="btn-small"
+              onClick={() => history.push("/signup")}
+            >
+              Sign Up
+            </Button>
+          </form>
+        </div>
+      </div>
+      <Message message={""} />
+      <div style={{ position: "relative", top: 180 }}>
+        <Message message={"Created by Chrysen Park"} />
+        <Message message={"© 2021 TheJarBar"} />
+      </div>
+    </Wrapper>
   );
 }
 
